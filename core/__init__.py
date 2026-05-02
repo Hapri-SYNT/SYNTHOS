bfe79c34c109ebc88a5fb8b5c71bd0a836f56950
Hapri-SYNT <alqalbai240@gmail.com>
2026-05-02 17:59:05 +0700

    core/__init__.py

---
+++ core/__init__.py
@@ -0,0 +1,3 @@
+from .infrastructure import memory_manager, epistemic_graph, auto_expand_graph, birth_dna_from_graph
+from .dna_sovereign import dna_pop, multi_brain, musyawarah, arbiter, heartbeat_daemon, action_safeguard, run_musyawarah_cycle
+from .economy import wallet_manager, gig_marketplace, universal_task_engine, alliance_manager, report_agent, run_universal_worker
